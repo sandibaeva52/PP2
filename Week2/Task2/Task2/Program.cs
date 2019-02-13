b8ca8407cfefcaba4806da22b3d07dbe9ba0cba1
Sandibayeva Aruzhan <sandibaeva52@gmail.com>
2019-02-14 02:11:22 +0300

    second commit

---
+++ Week2/Task2/Task2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace W3
+{
+    class Program
+    {
+        static bool Isprime(int n) //checks for the prime number
+        {
+            bool t = true;
+            if (n <= 1)
+            {
+                return false;
+            }
+            for (int i = 2; i <= n / 2; i++)
+            {
+                if (n % i == 0)
+                {
+                    return false;
+                }
+            }
+            return t;
+        }
+        static void Main(string[] args)
+        {
+            string path = @"C:\PP2\Week2\Task2\Task2\text.txt";
+            string path2 = @"C:\PP2\Week2\Task2\Task2\output.txt";
+            StreamReader sr = new StreamReader(path);//for reading the file 1 in path
+            StreamWriter sw = new StreamWriter(path2);//for writing to the file 2 in path2
+            String[] ss = sr.ReadToEnd().Split(' ');//read strings by space
+            foreach (string s in ss)//take each elements in file 1 
+            {
+                int q = int.Parse(s);//convert string to integer
+                if (Isprime(q))//checks the numbers in file 1 for the prime, if it is true
+                {
+                    sw.Write(q + " ");//write prime numbers to file 2
+                }
+            }
+            sw.Close();//close the file 2
+        }
+    }
+}
---
+++ Week2/Task4/Task4/Task4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string s1 = @"C:\PP2\Week2\Task4\Task4\first.txt"; // file path
+            string s2 = @"C:\PP2\Week2\Task4\Task4\second.txt";    
+            string s = "Just some string";
+            StreamWriter sw = new StreamWriter(s1);
+            sw.Write(s);
+            sw.Close();
+            File.Copy(s1, s2);//copy text from first to second
+            File.Delete(s1);//delete first file
+        }
+    }
+}
